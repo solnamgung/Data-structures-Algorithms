843e6c98f79c92d6e3ea860a6537adfc94a7859c
sol namgung <nandaras0103@gmail.com>
2022-05-07 23:37:39 +0900

    add array

--- src/com/dataStructuresAndAlgorithms/highArray/HighArray.java
+++ src/com/dataStructuresAndAlgorithms/highArray/HighArray.java
@@ -1,4 +1,48 @@
 package com.dataStructuresAndAlgorithms.highArray;
 
+import java.util.EnumMap;
+
 public class HighArray {
+    private long[] a;
+    private int nElements;
+
+    public HighArray(int max) {
+        a = new long[max];
+        nElements = 0;
+    }
+    public boolean find(long searchKey) { // find specified value
+        int j;
+        for(j = 0; j < nElements; j++)
+            if(a[j] == searchKey)
+                break;
+        if(j == nElements)
+            return false;
+        else
+            return true;
+    }
+    public void insert(long value) { //put element into array
+        a[nElements] = value;
+        nElements++;
+    }
+
+    public boolean delete(long value) {
+        int i;
+        for(i = 0; i < nElements; i++) // look for it
+            if(value == a[i] )
+                break;
+        if(i == nElements)  // cannot find it
+            return false;
+        else {
+            for(int k = i; k < nElements; k++) // move higher ones down
+                a[k] = a[k+1];
+            nElements--;  // decrement size
+            return true;
+        }
+    }
+
+    public void display() { //displays array contents
+        for(int i = 0; i < nElements; i++)
+            System.out.print( a[i] + ", " );
+        System.out.println("");
+    }
 }
---
+++ src/com/dataStructuresAndAlgorithms/highArray/HighArrayApp.java
@@ -0,0 +1,34 @@
+package com.dataStructuresAndAlgorithms.highArray;
+
+public class HighArrayApp {
+    public static void main(String[] args) {
+        int maxSize = 100;
+        HighArray arr = new HighArray(maxSize);
+
+        arr.insert(77);
+        arr.insert(11);
+        arr.insert(33);
+        arr.insert(22);
+        arr.insert(55);
+        arr.insert(44);
+        arr.insert(99);
+        arr.insert(00);
+        arr.insert(66);
+        arr.insert(88);
+
+        arr.display();
+
+        int searchKey = 39;
+        if(arr.find(searchKey))
+            System.out.println("Found " + searchKey);
+        else
+            System.out.println("Cannot find " + searchKey);
+
+        arr.delete(00);
+        arr.delete(11);
+        arr.delete(88);
+
+        arr.display();
+    }
+
+}
